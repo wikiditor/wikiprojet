cf3f9fe9567ddcbe401585a2534109d2278d4d1f
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-07-13 14:01:44 +0200

    update de la route

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/file')]
 class FileController extends AbstractController
 {
-    #[Route('/file', name: 'app_file')]
+    #[Route('/', name: 'app_file')]
     public function index(): Response
     {
         return $this->render('file/index.html.twig', [
             'controller_name' => 'FileController',
         ]);
     }
+
+    // #[Route('/file/list', name: 'app_file_list')]
+    // public function getListFiles(): Response
+    // {
+    //     return $this->render('file/index.html.twig', [
+    //         'controller_name' => 'FileController',
+    //     ]);
+    // }
+
 }
