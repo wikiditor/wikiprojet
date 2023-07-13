bfc29702bc78d36104cf7498488376e50211e7cc
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-07-13 14:59:02 +0200

    update de FileController et des vues associées

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
