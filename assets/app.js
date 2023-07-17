03528aba07c2436cae35883fd31ae6c112320be8
erika bal <erikabal75@hotmail.com>
2023-07-17 15:32:40 +0200

    installation de la barre de recherche wikipedia
    fonction pour chercher les articles avec un mot clé.
    fonction pour les différents languages.
    ucwords(mb_strtolower($searchTerm) pour changer les maj et minscule

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,12 @@
 import { Draggable } from '@shopify/draggable';
+// import { EditorJS } from '@editorjs/editorjs';
 
+console.log('COUCOU !');
 
 const draggable = new Draggable(document.querySelectorAll('ul'), {
     draggable: 'li'
 });
+console.log('coucou draggable', draggable);
 
-//console.log('coucou', draggable);
+//const editor = new EditorJS();
+// console.log('coucou', editor);
