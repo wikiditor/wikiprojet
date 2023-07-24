a05f53aa63968f79d29570966a8ded9b987588ce
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-07-24 18:02:16 +0200

    yo

--- src/Form/FileType.php
+++ src/Form/FileType.php
@@ -7,16 +7,20 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Document\File;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 
 class FileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TypeTextType::class, [
+                'attr' => ['value' => 'test']
+            ])
             ->add('content', TextareaType::class, [
-                'attr' => ['rows' => 5]
+                'attr' => ['rows' => 5, 'value' => 'test']
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer'
